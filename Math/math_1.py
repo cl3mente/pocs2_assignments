8c9e42bb638cab19465098b52c437fd1b92042f8
cl3mente <clemente.calabrese@gmail.com>
2022-11-01 14:07:09 +0100

    upload

--- Math/math_1.py
+++
@@ -1,7 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
-import cmath
-z = complex(input())
-
-print(abs(z))
-print(cmath.phase(z))
--- Sets/sets_4.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
-size_n = int(input())
-
-N = set()
-
-for i in range(size_n):
-    N.add(input())    
-print(len(N))
