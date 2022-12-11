a2d750bcdefdeaf25e40702acbb05219717fec34
cl3mente <clemente.calabrese@gmail.com>
2022-12-11 18:16:59 +0100

    upload

--- assignment_4/lgis.py
+++ assignment_4/lgis.py
@@ -3,16 +3,16 @@ with open("rosalind_lgis.txt") as f:
     n = int(f[0])
     pi = list(map(int, f[1].split()))
 
-inc = [(0,[])]*(n+1)
+arr = [(0,[])]*(n+1)
 for i in pi:
-    x,y = max(inc[:i])
-    inc[i] = (x+1,y+[i])
+    x,y = max(arr[:i])
+    arr[i] = (x+1,y+[i])
 
-print(*max(inc)[1])
+print(*max(arr)[1])
 
-inc = [(0,[])]*(n+1)
+arr = [(0,[])]*(n+1)
 for i in reversed(pi):
-    x,y = max(inc[:i])
-    inc[i] = (x+1,y+[i])
+    x,y = max(arr[:i])
+    arr[i] = (x+1,y+[i])
 
-print(*reversed(max(inc)[1]))
+print(*reversed(max(arr)[1]))
