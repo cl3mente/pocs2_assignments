ae7f50e7bba94d28e0482eaef1ce38852d0f354d
cl3mente <clemente.calabrese@gmail.com>
2022-12-24 13:56:41 +0100

    final upload

--- assignment_5/long.py
+++ assignment_5/long.py
@@ -1,46 +1,52 @@
 from Bio import SeqIO
+import sys
 
 with open("rosalind_long.txt") as h:
     reads = []
     for read in SeqIO.parse(h, "fasta"):
         reads.append(str(read.seq))
 
-result = ""
-
-print(*reads, sep="\n")
-
 def superstring(reads):
-    #this function takes a list and returns a list with pairwise superstrings (?)
-    match = []
+    #this function takes a list and returns a superstring
 
-    if len(reads) == 1:
-        return reads
-        #termination condition
-    else:
-        for i in reads:
-            for j in reads:
-                if i != j:
-                    
-                    #matching the start of a string i with the end of a string j
+    match = reads.pop(0)
+    index = 0
 
-                    k = len(i)//2
-                    scan = i[:k]
+    while reads != []:
 
-                    while scan in j and k <= len(i):
-                        k += 1
-                        scan = i[:k]
-                    match.append(j + i[k:])
+        i = reads[index]
+        k = len(i)//2
 
-                    #matching the end of a string with the start of a string
+        #matching the start of a string i with the end of a string j
 
-                    k = len(i)//2
-                    scan = i[k:]
+        scan = i[:k]
 
-                    while scan in j and k >= 0:
-                        k -= 1
-                        scan = i[k:]
-                    match.append(i[:k] + j)
-        return superstring(match)
+        if scan in match:
+            match = match[:match.index(scan)] + i
+            reads.remove(i)
+            index = 0
+            continue
+        
+
+        #matching the end of a string with the start of a string
+
+        scan = i[k:]
+
+        if scan in match:
+            match = i + match[match.index(scan) + len(scan):]
+            reads.remove(i)
+            index = 0
+            continue
+        else:        
+            #increment
+            index += 1
+    
+    return match
+
+with open("answer.txt", "x") as h:
+    sys.stdout = h
+    print(superstring(reads))
 
 
-print(superstring(reads))
+# risultato = ATTAGACCTGCCGGAATAC
+# my result = ATTAGACCTGCCGGAATAC
