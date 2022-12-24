cd0e9b14c5f56fb84df66e8d3a341b9194c2b7fc
cl3mente <clemente.calabrese@gmail.com>
2022-12-24 11:37:10 +0100

    upload

--- assignment_5/tree.py
+++ assignment_5/tree.py
@@ -1,5 +1,12 @@
 with open("rosalind_tree.txt") as f:
     n, *adj = f.read().splitlines()
     adj = [tuple(i.split()) for i in adj]
+    n = int(n)
 
 print(n, adj)
+
+#literally a tree has to have n-1 edges
+#simple arithmetic solution
+
+result = n - len(adj) - 1
+print(result)
